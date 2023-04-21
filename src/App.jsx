c54a3fd2a58f5436251280280aa3e1cd1944bec5
pavanmohinkar097 <pavanmohi097@gmail.com>
2023-04-21 18:39:36 +0530

    21-04-2023

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+//Normal function//
+function App() {
+  return (
+    <div>
+      <header>hi babu</header>
+      <main>GTY</main>
+      <footer>nin hbdb</footer>
+    </div>
+  );
+}
+export default App;
+
+// <App /> --------> <h1>Hello mom</h1>
